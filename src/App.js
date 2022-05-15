bc1a065d388dfc11aab7e7e72b302113211495b6
petergachau <83207831+petergachau@users.noreply.github.com>
2022-05-15 12:08:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
+
+function App() {
+  
+  return (
+    <>
+      <nav>
+        <h4> rosa</h4>
+<ul>
+    <li>
+        home
+    </li>
+    <li>
+        about
+    </li>
+</ul>
+    </nav>
+    <h1>Rosa sales site</h1>
+    <h3>send your order</h3>
+    <div className="section">
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img className="img" src="https://res.cloudinary.com/pitz/image/upload/v1652603603/samples/animals/Snapchat-1393282058_jn0evm.jpg" alt=""/>
+
+            <h5> date: 14 june 2021</h5>
+            day:sato
+            <br></br>
+            location:nairobi
+        </div>
+         <div className="card">
+            <h3>rosas photos</h3>
+            <img src="" alt=""/>
+        </div> 
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img className="img" src="https://res.cloudinary.com/pitz/image/upload/v1652603785/samples/animals/IMG_20211114_143322_agyphw.jpg" alt=""/>
+
+            <h5> date: 14 june 2021</h5>
+            day:sato
+            <br></br>
+            location:nairobi
+
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img className="img" src="https://res.cloudinary.com/pitz/image/upload/v1652347020/peter/dcvlmbjkrf10fdtvu0vs.png" alt=""/>
+
+            <h5> date: 14 june 2021</h5>
+            day:sato
+            <br></br>
+            location:nairobi
+            
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img className="img" src="https://res.cloudinary.com/pitz/image/upload/v1652603604/samples/animals/Snapchat-1785481603_bsp9gi.jpg" alt=""/>
+
+            <h5> date: 14 june 2021</h5>
+            day:sato
+            <br></br>
+            location:nairobi
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img className="img" src="https://res.cloudinary.com/pitz/image/upload/v1643945433/samples/animals/kitten-playing.gif" alt=""/>
+
+            <h5> date: 14 june 2021</h5>
+            day:sato
+            <br></br>
+            location:nairobi
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img src="" alt=""/>
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img src="" alt=""/>
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img src="" alt=""/>
+        </div>
+        <div className="card">
+            <h3>rosas photos</h3>
+            <img src="" alt=""/>
+        </div>
+        
+        </div>
+        <footer>
+            <h6>
+                developed by @rosa
+            </h6>
+        </footer>
+    
+    </>
+  
+  );
+}
+
+export default App;
